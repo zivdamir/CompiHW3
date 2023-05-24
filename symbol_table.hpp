733e507e708e34afa228402a43a23dfcf3cb6a3a
zivdamir <zivdamir@campus.technion.ac.il>
2023-05-24 13:07:26 +0300

    hello levi

---
+++ symbol_table.hpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include "table_entry.hpp"
+using namespace std;
+
+class symbol_table_entry{
+    public:
+        symbol_table_entry *parent;
+        vector<table_entry*> entries;
+        bool is_global_scope;
+        symbol_table_entry(bool is_global_scope = false, symbol_table_entry *parent = nullptr) : parent(parent),is_global_scope(is_global_scope)
+        {
+        }
+    //need to add Table~
+    void insert(string name,string type,int offset,bool is_func){
+        assert(!(!this->is_global_scope && is_func));//idk if should be condition or not todo
+        if (!is_func)
+        {
+            if(contains_in_current_scope(name,type))
+            {
+                return;// maybe return error or something.
+            }
+            else{
+                table_entry *new_entry = new table_entry(name, offset, type);
+                if(!new_entry){
+                    assert (false); //TODO
+                }
+                entries.push_back(new_entry);
+            }
+        }
+
+        return;
+    }
+    bool contains_in_current_scope(string name, string type){
+        bool found = false;
+        if (!entries.empty())
+        {
+            for (table_entry* entry: entries){
+                if(entry->name == name && entry->type == type){
+                    found = true;
+                    break;
+                }
+            }
+        }
+        return found;
+    }
+    
+};
