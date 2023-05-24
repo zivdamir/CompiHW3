3c579a3b0d603467f247ea8b78ee5da26b412572
zivdamir <zivdamir@campus.technion.ac.il>
2023-05-24 14:10:34 +0300

    changes like you wanted

--- symbol_tables_stack.hpp
+++ symbol_tables_stack.hpp
@@ -12,14 +12,23 @@ class symbol_tables_stack{
         stack<symbol_table*> tables;
         stack<int> offsets;
         symbol_tables_stack():tables(),offsets(){
-            push_scope();//init global scope
+            symbol_table *global_scope = Maketable(true,nullptr);
+            push(offsets, 0);
+            push(tables, global_scope); // init global scope
             //1.do we want to init a global scope with te construction
             //2. levi needs to undetrstands pop deletes the object!
             // inWhile = false;
         }//init tables and offset
-
-        void push_scope(){
-            symbol_table *parent= nullptr;
+        static symbol_table* Maketable(bool is_global = false,symbol_table *parent)
+        {
+            return new symbol_table(is_global,parent);
+        }
+        template <class T>
+        void push(stack<T> stack,T object)
+        {
+            stack.push(object);
+            return;
+             /*symbol_table *parent = nullptr;
             bool is_global = true;
             int offset = 0;
             if (!tables.empty())
@@ -34,19 +43,20 @@ class symbol_tables_stack{
                 assert(false);//bad allocation
             }
             this->tables.push(new_scope);
-            this->offsets.push(offset);
-            return;
+            this->offsets.push(offset);*/
         }
-        void pop_scope(){
+        void pop(){
+            //unless you want to make it super complicated, this stays this way!
             symbol_table *top_table = this->tables.top();
             this->tables.pop();
             this->offsets.pop();
             delete top_table;
             return;
         }
-        symbol_table* top_scope()
+        template <class T>
+        T top(stack<T> stack)
         {
-            return this->tables.top();
+            return stack.top();
         }
         symbol_table* get_global_scope(){
             //returns the global_scope to find functions and global variables and such
